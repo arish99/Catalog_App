83936515456b3dd7cc49f883d2c4ebce4eb1c823
arish <arish.ahmad111@gmail.com>
2021-06-26 00:13:33 +0530

    Day 9 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: Text(
+          "Catalog App",
+        ),
       ),
       drawer: MyDrawer(),
       body: Center(
